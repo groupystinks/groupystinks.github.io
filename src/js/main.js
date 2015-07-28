4a02a193a5086624007ee0f22f61461568a756d1
Groupystinks <jason0911520@gmail.com>
2015-07-28 15:43:40 +0800

    publish production

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-function run() {
-  var React = require('react');
-  var router = require('./router');
-  router.run((Root, state) => {
-    React.render(<Root path={state.path} />, document.body);
-  });
-}
-
-run();
